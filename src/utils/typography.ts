7b32e0136302f9d910ca237d12bd535242829975
Brandon Ling <brandonvling@gmail.com>
2020-07-21 15:15:21 -0400

    Add Source Sans Pro font

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,17 +1,4 @@
 import Typography from "typography"
-// import Wordpress2016 from "typography-theme-wordpress-2016"
-
-// Wordpress2016.overrideThemeStyles = () => {
-//   return {
-//     "a.gatsby-resp-image-link": {
-//       boxShadow: `none`,
-//     },
-//   }
-// }
-
-// delete Wordpress2016.googleFonts
-
-// const typography = new Typography(Wordpress2016)
 export const baseTextColor = "#fdfff5"
 export const baseBackgroundColor = "#333333"
 export const baseAccentColor = "#B88BDA"
