822d06453b0cb981d28c6648387a45c45103d502
Brandon Ling <brandonvling@gmail.com>
2021-01-28 12:33:49 -0500

    Add IQR error bars

--- content/blog/drafts/queue-implementations/src/MedianDequeuePlot.tsx
+++ content/blog/drafts/queue-implementations/src/MedianDequeuePlot.tsx
@@ -1,6 +1,7 @@
-import { median as d3Median, scaleLinear } from "d3"
+import { median as d3Median, scaleLinear, quantile } from "d3"
 import { Scatterplot } from "./components/Scatterplot"
 import { colors } from "./colors"
+import { Fragment } from "react"
 
 const MedianDequeuePlot = ({
   datasets,
@@ -16,8 +17,17 @@ const MedianDequeuePlot = ({
   y: number
 }) => {
   const logMedianTimes = datasets.map(ds => ({
-    logMedianDequeueTime: d3Median(ds, d => Math.log10(d.dequeueTime)),
+    logMedianDequeueTime: d3Median(ds, d => Math.log10(d.dequeueTime))!,
+    firstQuartile: quantile(ds, 0.25, d => Math.log10(d.dequeueTime))!,
+    thirdQuartile: quantile(ds, 0.75, d => Math.log10(d.dequeueTime))!,
   }))
+  const xScale = scaleLinear()
+    .domain([2, 7])
+    .nice()
+    .range([0, width - 140])
+  const yScale = scaleLinear()
+    .domain([1, 8])
+    .range([height - 40, 40])
   return (
     <svg width={width} height={height}>
       <Scatterplot
@@ -34,17 +44,72 @@ const MedianDequeuePlot = ({
             .slice(12, 19)
             .map((d, i) => [i + 2, d.logMedianDequeueTime]),
         ]}
-        xScale={scaleLinear()
-          .domain([2, 7])
-          .nice()
-          .range([0, width - 140])}
-        yScale={scaleLinear()
-          .domain([1, 8])
-          .range([height - 40, 40])}
+        xScale={xScale}
+        yScale={yScale}
         xLabel={"log n"}
         yLabel={"log t"}
         colors={colors}
       />
+      {logMedianTimes.slice(0, 6).map((ds, i) => {
+        return (
+          <Fragment key={ds.logMedianDequeueTime}>
+            <line
+              stroke={colors[0]}
+              x1={65 + xScale(i + 2)}
+              x2={65 + xScale(i + 2)}
+              y1={yScale(ds.logMedianDequeueTime)}
+              y2={yScale(ds.thirdQuartile)}
+            />
+            <line
+              stroke={colors[0]}
+              x1={65 + xScale(i + 2)}
+              x2={65 + xScale(i + 2)}
+              y1={yScale(ds.logMedianDequeueTime)}
+              y2={yScale(ds.firstQuartile)}
+            />
+          </Fragment>
+        )
+      })}
+      {logMedianTimes.slice(6, 12).map((ds, i) => {
+        return (
+          <Fragment key={ds.logMedianDequeueTime}>
+            <line
+              stroke={colors[0]}
+              x1={65 + xScale(i + 2)}
+              x2={65 + xScale(i + 2)}
+              y1={yScale(ds.logMedianDequeueTime)}
+              y2={yScale(ds.thirdQuartile)}
+            />
+            <line
+              stroke={colors[1]}
+              x1={65 + xScale(i + 2)}
+              x2={65 + xScale(i + 2)}
+              y1={yScale(ds.logMedianDequeueTime)}
+              y2={yScale(ds.firstQuartile)}
+            />
+          </Fragment>
+        )
+      })}
+      {logMedianTimes.slice(12, 19).map((ds, i) => {
+        return (
+          <Fragment key={ds.logMedianDequeueTime}>
+            <line
+              stroke={colors[2]}
+              x1={65 + xScale(i + 2)}
+              x2={65 + xScale(i + 2)}
+              y1={yScale(ds.logMedianDequeueTime)}
+              y2={yScale(ds.thirdQuartile)}
+            />
+            <line
+              stroke={colors[0]}
+              x1={65 + xScale(i + 2)}
+              x2={65 + xScale(i + 2)}
+              y1={yScale(ds.logMedianDequeueTime)}
+              y2={yScale(ds.firstQuartile)}
+            />
+          </Fragment>
+        )
+      })}
     </svg>
   )
 }
--- content/blog/drafts/queue-implementations/src/MedianEnqueuePlot.tsx
+++ content/blog/drafts/queue-implementations/src/MedianEnqueuePlot.tsx
@@ -1,7 +1,7 @@
-import { median as d3Median, scaleLinear } from "d3"
+import { median as d3Median, quantile, scaleLinear } from "d3"
 import { Scatterplot } from "./components/Scatterplot"
 import { colors } from "./colors"
-
+import { Fragment } from "react"
 const MedianEnqueuePlot = ({
   datasets,
   height,
@@ -16,9 +16,17 @@ const MedianEnqueuePlot = ({
   y: number
 }) => {
   const logMedianTimes = datasets.map(ds => ({
-    logMedianEnqueueTime: d3Median(ds, d => Math.log10(d.enqueueTime)),
+    logMedianEnqueueTime: d3Median(ds, d => Math.log10(d.enqueueTime))!,
+    firstQuartile: quantile(ds, 0.25, d => Math.log10(d.enqueueTime))!,
+    thirdQuartile: quantile(ds, 0.75, d => Math.log10(d.enqueueTime))!,
   }))
-
+  const xScale = scaleLinear()
+    .domain([2, 7])
+    .nice()
+    .range([0, width - 140])
+  const yScale = scaleLinear()
+    .domain([1, 4])
+    .range([height - 40, 40])
   return (
     <svg width={width} height={height}>
       <Scatterplot
@@ -35,17 +43,32 @@ const MedianEnqueuePlot = ({
             .slice(12, 19)
             .map((d, i) => [i + 2, d.logMedianEnqueueTime]),
         ]}
-        xScale={scaleLinear()
-          .domain([2, 7])
-          .nice()
-          .range([0, width - 140])}
-        yScale={scaleLinear()
-          .domain([1, 4])
-          .range([height - 40, 40])}
+        xScale={xScale}
+        yScale={yScale}
         xLabel={"log n"}
         yLabel={"log t"}
         colors={colors}
       />
+      {logMedianTimes.slice(0, 6).map((ds, i) => {
+        return (
+          <Fragment key={ds.logMedianEnqueueTime}>
+            <line
+              stroke={colors[0]}
+              x1={65 + xScale(i + 2)}
+              x2={65 + xScale(i + 2)}
+              y1={yScale(ds.logMedianEnqueueTime)}
+              y2={yScale(ds.thirdQuartile)}
+            />
+            <line
+              stroke={colors[0]}
+              x1={65 + xScale(i + 2)}
+              x2={65 + xScale(i + 2)}
+              y1={yScale(ds.logMedianEnqueueTime)}
+              y2={yScale(ds.firstQuartile)}
+            />
+          </Fragment>
+        )
+      })}
     </svg>
   )
 }
