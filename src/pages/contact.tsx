714dd803d6087cebf61e89dc4b4a8ff1fa8a0dd7
Brandon Ling <brandonvling@gmail.com>
2022-08-03 14:37:29 -0400

    Change the way email is displayed

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,14 +1,13 @@
-import Layout, { defaultStyle } from '../components/layout'
-import { mediaQueries } from '../utils/media-queries'
-import SEO from '../components/seo'
-import { css } from '@emotion/react'
-import { graphql, PageProps } from 'gatsby'
+import Layout, { defaultStyle } from "../components/layout"
+import { mediaQueries } from "../utils/media-queries"
+import SEO from "../components/seo"
+import { css } from "@emotion/react"
+import { graphql, PageProps } from "gatsby"
 
 interface ContactPageData {
   site: {
     siteMetadata: {
       title: string
-      email: string
       github: string
       linkedIn: string
     }
@@ -31,17 +30,15 @@ const contactPageStyles = css`
   }
 `
 
-export default function Contact ({ data }: PageProps<ContactPageData>) {
-  const { title: siteTitle, email, github, linkedIn } = data.site.siteMetadata
+export default function Contact({ data }: PageProps<ContactPageData>) {
+  const { title: siteTitle, github, linkedIn } = data.site.siteMetadata
 
   return (
     <Layout title={siteTitle} style={contactPageStyles}>
       <SEO title={siteTitle} />
       <p>Feel free to send me a message through:</p>
       <ul>
-        <li>
-          Email: <a href={`mailto:${email}`}>{email}</a>
-        </li>
+        <li>Email: Gmail domain with local-part brandonling.dev</li>
         <li>
           LinkedIn: <a href={`https://${linkedIn}`}>{linkedIn}</a>
         </li>
@@ -58,7 +55,6 @@ export const contactPageQuery = graphql`
     site {
       siteMetadata {
         title
-        email
         github
         linkedIn
       }
